a5159bde943b820bfe8d02a7b333390187f8922e
Mike <mclancy96@gmail.com>
2025-06-24 12:24:44 -0400

    Add more money form

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
+import WalletButton from "./WalletButton";
 
 const API = "http://localhost:3001/sushis";
 
@@ -15,16 +16,32 @@ function App() {
       .then(setSushi);
   }, []);
   return (
-    <div className="app">
-      <SushiContainer
-        sushi={sushi}
-        displayedSushiIndex={displayedSushiIndex}
-        setDisplayedSushiIndex={setDisplayedSushiIndex}
-        setEatenSushiPlates={setEatenSushiPlates}
-        budget={budget}
-        setBudget={setBudget}
-      />
-      <Table plates={eatenSushiPlates} budget={budget} />
+    <div
+      className="app-container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+      }}
+    >
+      <div className="app" style={{ width: "100%", maxWidth: "800px" }}>
+        <SushiContainer
+          sushi={sushi}
+          displayedSushiIndex={displayedSushiIndex}
+          setDisplayedSushiIndex={setDisplayedSushiIndex}
+          setEatenSushiPlates={setEatenSushiPlates}
+          budget={budget}
+          setBudget={setBudget}
+        />
+        <Table plates={eatenSushiPlates} budget={budget} />
+      </div>
+      <div
+        style={{ marginTop: "20px" }}
+      >
+        <WalletButton setBudget={setBudget} />
+      </div>
     </div>
   );
 }
---
+++ src/components/WalletButton.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function WalletButton({ setBudget }) {
+  const [isFormShown, setIsFormShown] = useState(false);
+  const [increaseAmount, setIncreaseAmount] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setBudget((oldBudget) => oldBudget + Number(increaseAmount));
+  };
+
+  const handleClick = () => {
+    setIsFormShown((iFH) => !iFH);
+  };
+  return (
+    <div
+      style={{ marginLeft: "auto", marginRight: "auto", textAlign: "center" }}
+    >
+      <button onClick={handleClick}>More money!</button>
+      {isFormShown && (
+        <form onSubmit={handleSubmit}>
+          <label>Add Amount to Wallet: </label>
+          <input
+            type="number"
+            placeholder="0"
+            value={increaseAmount}
+            onChange={(e) => setIncreaseAmount(e.target.value)}
+            step={0.01}
+          ></input>
+          <button>Add Money</button>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default WalletButton;
